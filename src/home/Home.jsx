4d2f216c0f254cf48797434971a28da505e57c1e
lucasimprovise <lucas.sevault@ynov.com>
2022-04-05 22:21:25 +0200

    Creation des sliders pour les films

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,6 +1,7 @@
 import './home.scss'
 
 import Featured from '../components/featured/Featured'
+import List from '../components/list/List'
 import Navbar from '../components/navbar/Navbar'
 
 const Home = () => {
@@ -8,7 +9,10 @@ const Home = () => {
         <div className="home">
             <Navbar/>
           <Featured type="movie"/>
-
+          <List/>
+          <List/>
+          <List/>
+          <List/>
         </div>
     )
 }
